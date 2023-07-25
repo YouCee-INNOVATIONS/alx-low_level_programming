9c0167c8626c6312ece2fe1d235d72c0c6dd26c2
Virtue Azuka <virtueucheskyazuka@gmail.com>
2023-07-25 07:37:44 -0700

    main.h updated, main.c first commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,14 +1,17 @@
-#include "main.h"
-#include <unistd.h>
+#ifndef main_h
+#define main_h
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _putchar(char c);
+
+#endif
